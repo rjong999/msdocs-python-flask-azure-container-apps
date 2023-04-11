6958adf44868dc77984a7cf71ab5a505fd7d991d
rjong999 <rjong999@hotmail.com>
2023-04-11 15:08:08 -0700

    test

--- app.py
+++ app.py
@@ -1,141 +1,44 @@
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_wtf.csrf import CSRFProtect
-from datetime import datetime
-import os
-import uuid
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
 
-from requests import RequestException
+app = Flask(__name__)
+api = Api(app)
 
-app = Flask(__name__, static_folder='static')
-csrf = CSRFProtect(app)
+STUDENTS = {
+    '1': {'name':'Robert', 'age':64, 'spec':'Azure'},
+    '2': {'name':'John', 'age':54, 'spec':'SQL'},
+    '3': {'name':'Mariella', 'age':42, 'spec':'Architect'}
+}
 
-# If RUNNING_IN_PRODUCTION is defined as an environment variable, then we're running on Azure
-if not 'RUNNING_IN_PRODUCTION' in os.environ:
-   # Local development, where we'll use environment variables.
-   print("Loading config.development and environment variables from .env file.")
-   app.config.from_object('azureproject.development')
-else:
-   # Production, we don't load environment variables from .env file but add them as environment variables in Azure.
-   print("Loading config.production.")
-   app.config.from_object('azureproject.production')
+SPECIALITIES = {
+    '1' : {'name':'Azure AD'},
+    '2' : {'name':'SQL'}
+}
+parser = reqparse.RequestParser()
 
-with app.app_context():
-    app.config.update(
-        SQLALCHEMY_DATABASE_URI=app.config.get('DATABASE_URI'),
-        SQLALCHEMY_TRACK_MODIFICATIONS=False,
-    )
+class StudentList(Resource):
+    def get(self):
+        return STUDENTS 
+    def post(self):
+        parser.add_argument("name", type=str, location='args')
+        parser.add_argument("age", type=int, location='args')
+        parser.add_argument("spec", type=str, location='args')
+        args = parser.parse_args()
+        student_id = int(max(STUDENTS.keys())) + 1
+        student_id = '%i' % student_id
+        STUDENTS[student_id] = {
+            "name": args["name"],
+            "age": args["age"],
+            "spec": args["spec"],
+            }
+        return STUDENTS[student_id], 201
 
-# Initialize the database connection
-db = SQLAlchemy(app)
+class SpecList(Resource):
+    def get(self):
+         return SPECIALITIES
 
-# Enable Flask-Migrate commands "flask db init/migrate/upgrade" to work
-migrate = Migrate(app, db)
-
-# Create databases, if databases exists doesn't issue create
-# For schema changes, run "flask db migrate"
-from models import Restaurant, Review
-with app.app_context():
-    db.create_all()
-    db.session.commit()
-
-@app.route('/', methods=['GET'])
-def index():
-    from models import Restaurant
-    print('Request for index page received')
-    restaurants = Restaurant.query.all()    
-    return render_template('index.html', restaurants=restaurants)
-
-@app.route('/<int:id>', methods=['GET'])
-def details(id):
-    return details(id,'')
-
-def details(id, message):
-    from models import Restaurant, Review
-    restaurant = Restaurant.query.where(Restaurant.id == id).first()
-    reviews = Review.query.where(Review.restaurant==id)
-    return render_template('details.html', restaurant=restaurant, reviews=reviews, message=message)
-
-@app.route('/create', methods=['GET'])
-def create_restaurant():
-    print('Request for add restaurant page received')
-    return render_template('create_restaurant.html')
-
-@app.route('/add', methods=['POST'])
-@csrf.exempt
-def add_restaurant():
-    from models import Restaurant
-    try:
-        name = request.values.get('restaurant_name')
-        street_address = request.values.get('street_address')
-        description = request.values.get('description')
-        if (name == "" or description == "" ):
-            raise RequestException()
-    except (KeyError, RequestException):
-        # Redisplay the restaurant entry form.
-        return render_template('create_restaurant.html', 
-            message='Restaurant not added. Include at least a restaurant name and description.')
-    else:
-        restaurant = Restaurant()
-        restaurant.name = name
-        restaurant.street_address = street_address
-        restaurant.description = description
-        db.session.add(restaurant)
-        db.session.commit()
-
-        return redirect(url_for('details', id=restaurant.id))
-
-@app.route('/review/<int:id>', methods=['POST'])
-@csrf.exempt
-def add_review(id):
-    from models import Review
-    try:
-        user_name = request.values.get('user_name')
-        rating = request.values.get('rating')
-        review_text = request.values.get('review_text')   
-        if (user_name == "" or rating == None):
-            raise RequestException()
-    except (KeyError, RequestException):
-        # Redisplay the review form.
-        from models import Restaurant
-        restaurant = Restaurant.query.where(Restaurant.id == id).first()
-        reviews = Review.query.where(Review.restaurant==id)
-        return details(id, 'Review not added. Include at least a name and rating for review.')
-    else:
-        review = Review()
-        review.restaurant = id
-        review.review_date = datetime.now()
-        review.user_name = user_name
-        review.rating = int(rating)
-        review.review_text = review_text
-        db.session.add(review)
-        db.session.commit()
-                
-    return redirect(url_for('details', id=id))        
-
-@app.context_processor
-def utility_processor():
-    def star_rating(id):
-        from models import Review
-        reviews = Review.query.where(Review.restaurant==id)
-
-        ratings = []
-        review_count = 0;        
-        for review in reviews:
-            ratings += [review.rating]
-            review_count += 1
-
-        avg_rating = round(sum(ratings)/len(ratings), 2) if ratings else 0
-        stars_percent = round((avg_rating / 5.0) * 100) if review_count > 0 else 0
-        return {'avg_rating': avg_rating, 'review_count': review_count, 'stars_percent': stars_percent}
-
-    return dict(star_rating=star_rating)
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+api.add_resource(StudentList, '/students')
+api.add_resource(SpecList, '/specialities')
 
 if __name__ == '__main__':
-   app.run()
+	app.run(host="0.0.0.0")
